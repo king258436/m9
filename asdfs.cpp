2b745cf286800396fdd7231bd94cafd87b920134
최원식 <king258436@naver.com>
2023-12-02 17:21:06 +0900

    Create asdfs.cpp

---
+++ asdfs.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int getParent(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = getParent(parent[x]);
+}
+
+void unionParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
+    if (a > b)parent[a] = b;
+    else parent[b] = a;
+}
+
+bool findParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
+    if (a == b) return true;
+    else return false;
+}
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+    for (int i = 0; i < E; i++) {
+        edge e;
+        cin >> e.v >> e.u >> e.dis;
+        vec.push_back(e);
+    }
+    sort(vec.begin(), vec.end(), comp);
+    for (int i = 1; i <= V; i++) {
+        parent[i] = i;
+    }
+    int sum = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        if (!findParent(vec[i].v, vec[i].u)) {
+            unionParent(vec[i].v, vec[i].u);
+            sum += vec[i].dis;
+        }
+    }
+    cout << sum;
+}
